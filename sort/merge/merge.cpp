43de0394592418d973e175e07eeef89adcb426ad
SamvelMakaryan <samvelmakaryan721@gmail.com>
2023-04-28 02:01:15 +0400

    adding merge sort

---
+++ sort/merge/merge.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+template <typename T>
+void merge(T* arr, int left, int mid, int right) {
+	int n1 = mid - left + 1;
+	int n2 = right - mid;
+	T* arr1 = new T [n1];
+	T* arr2 = new T [n2];
+	for (int i = 0; i < n1; ++i) {
+		arr1[i] = arr[left + i];
+	}
+	for (int i = 0; i < n2; ++i) {
+		arr2[i] = arr[mid + 1 + i];
+	}
+	int i = 0;
+	int j = 0;
+	int k = left;
+	while (i < n1 && j < n2) {
+		if (arr1[i] >= arr2[j]) {
+			arr[k] = arr2[j++];
+		}
+		else {
+			arr[k] = arr1[i++];
+		}
+		++k;
+	}
+	while (i < n1) {
+		arr[k++] = arr1[i++];
+	}
+	while(j < n2) {
+		arr[k++] = arr2[j++];
+	}
+	delete[] arr1;
+	delete[] arr2;
+}
+
+template <typename T>
+void mergesort(T arr, int left, int right) {
+	if (left >= right) {
+		return;
+	}
+	int mid = left + (right - left) / 2;
+	mergesort(arr, left, mid);
+	mergesort(arr, mid + 1, right);
+	merge(arr, left, mid, right);
+}
+
+
+
+int main() {
+	int arr[] = {32, 45, 6, 7, -8, 7, 55, 2, 11, 20};
+	mergesort(arr, 0, 9);
+	for (auto& i: arr) {
+		std::cout << i << "  ";
+	}
+}
+
+// Time complexity O(n log n)
+// Space complexity O(n)
